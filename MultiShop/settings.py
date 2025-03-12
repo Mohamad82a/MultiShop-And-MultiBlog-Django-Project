8d0bf1c3ed5990225bcdb4b7f8d6a12cde8509fc
Mohamad82a <mohamad2003abasi@gmail.com>
2025-03-12 19:09:41 +0330

    Update settings.py

--- MultiShop/settings.py
+++ MultiShop/settings.py
@@ -142,8 +142,10 @@ LOGIN_URL = '/account/login'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = '/home/multishop/public_html/media'
+STATIC_ROOT = '/home/multishop/public_html/staticfiles'
 
 
 # Default primary key field type
